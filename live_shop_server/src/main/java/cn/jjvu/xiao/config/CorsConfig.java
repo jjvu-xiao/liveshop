49e14f79f4eaf4cddbf0f179218ccd72c19c611a
Mr Xiao <m18779334084@163.com>
2020-06-01 15:23:25 +0800

    Spring Boot服务端搭建

--- live_shop_server/src/main/java/cn/jjvu/xiao/config/CorsConfig.java
+++ live_shop_server/src/main/java/cn/jjvu/xiao/config/CorsConfig.java
@@ -1,4 +1,28 @@
 package cn.jjvu.xiao.config;
 
-public class CorsConfig {
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * CORS 跨域配置
+ */
+public class CorsConfig implements WebMvcConfigurer {
+
+    /**
+     *  允许所有跨域访问的路径
+     *  允许所有跨域访问的源
+     *  允许 POST GET PUT DELETE OPTIONS 方法请求
+     *  预检间隔时间为 168秒
+     *  允许头部设置
+     *  允许发送Cookie
+     */
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
+                .maxAge(168000)
+                .allowedMethods("*")
+                .allowCredentials(true);
+    }
 }
