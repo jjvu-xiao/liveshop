8474e9243caabcb922c595943dd72927f909c53d
Mr Xiao <m18779334084@163.com>
2020-06-09 17:27:06 +0800

    增加了Spring AOP 环绕日志功能，增加了JWT 与 Spring Security 安全认证服务

---
+++ live_shop_server/src/main/java/cn/jjvu/xiao/core/security/JwtAuthenticationFilter.java
@@ -0,0 +1,35 @@
+package cn.jjvu.xiao.core.security;
+
+import java.io.IOException;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import cn.jjvu.xiao.utils.SecurityUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+/**
+ * 登录认证过滤器
+ * @author Louis
+ * @date Jan 14, 2019
+ */
+public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
+
+	
+	@Autowired
+    public JwtAuthenticationFilter(AuthenticationManager authenticationManager) {
+        super(authenticationManager);
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+    	// 获取token, 并检查登录状态
+        SecurityUtils.checkAuthentication(request);
+        chain.doFilter(request, response);
+    }
+    
+}
---
+++ live_shop_server/src/main/java/cn/jjvu/xiao/core/security/JwtAuthenticationProvider.java
@@ -0,0 +1,39 @@
+package cn.jjvu.xiao.core.security;
+
+import cn.jjvu.xiao.utils.PasswordEncoder;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+/**
+ * 身份验证提供者
+ * @author Louis
+ * @date Jan 14, 2019
+ */
+public class JwtAuthenticationProvider extends DaoAuthenticationProvider {
+
+    public JwtAuthenticationProvider(UserDetailsService userDetailsService) {
+        setUserDetailsService(userDetailsService);
+    }
+
+    @Override
+	protected void additionalAuthenticationChecks(UserDetails userDetails, UsernamePasswordAuthenticationToken authentication)
+			throws AuthenticationException {
+		if (authentication.getCredentials() == null) {
+			logger.debug("Authentication failed: no credentials provided");
+			throw new BadCredentialsException(messages.getMessage("AbstractUserDetailsAuthenticationProvider.badCredentials", "Bad credentials"));
+		}
+
+		String presentedPassword = authentication.getCredentials().toString();
+		String salt = ((JwtUserDetails) userDetails).getSalt();
+		// 覆写密码验证逻辑
+		if (!new PasswordEncoder(salt).matches(userDetails.getPassword(), presentedPassword)) {
+			logger.debug("Authentication failed: password does not match stored value");
+			throw new BadCredentialsException(messages.getMessage("AbstractUserDetailsAuthenticationProvider.badCredentials", "Bad credentials"));
+		}
+	}
+
+}
